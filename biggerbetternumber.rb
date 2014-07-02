3ead19bad5814bc2cc23c9e4730b87200426db79
William Mahoney <wm.j.mahoney@gmail.com>
2014-07-02 17:22:33 -0400

    bigger better number
    I liked the latter version which included # {} and double quotes, only
    assigns one variable.

---
+++ biggerbetternumber.rb
@@ -0,0 +1,3 @@
+puts 'Good day, What is your favorite number??'
+favnumber = gets.chomp.to_i
+puts "While that is a great number... might I suggest #{favnumber + 1}?  #{favnumber + 1} is a much better number"
